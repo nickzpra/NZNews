3d7f0423da80eceb0b21351bf97fa05a47aedcab
nickzpra <nickzpra@gmail.com>
2024-10-27 21:12:09 -0700

    change

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:5039/api/News'//https://nznewsapi.azurewebsites.net/api/news'
-   // http://localhost:5039/api/News
+    apiUrl: 'http://localhost:5039/api/News'
   };
